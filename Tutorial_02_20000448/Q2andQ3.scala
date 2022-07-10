2c6bcfa3f65ba592a08512afc9de7d324c9efb60
Nirupama Dissanayake <nirupamadissanayake23@gmail.com>
2022-07-10 22:56:22 +0530

    adding tutorial02

---
+++ Tutorial_02_20000448/Q2andQ3.scala
@@ -0,0 +1,33 @@
+object Q2andQ3 extends App{
+
+  //Question 02
+
+  var k, i, j:Int = 2
+  var m, n:Int = 5
+  var f:Float = 12.0f
+  var g:Float = 4.0f
+  var c:Char = 'X'
+
+  //Question 03
+
+  println {"a) " + (k + 12 * m)}
+  println {"b) " + (m/j)}
+  println {"c) " + (n%j)}
+  println {"d) " + (m/j*j)}
+  println {"e) " + (f+10*5+g)}
+  //println {"f) " + ((++i)*n)}
+
+  var a:Int=2
+  var b:Int=3
+  var c2:Int=4   //c and k are already defined, c as a 'char' variable and k as an 'int' variable
+  var d:Int=5
+  var k2:Float=4.3f
+
+  println()
+  //println("a) " + (--b*a+c2*d--))   //scala does not support prefix and postfix
+  //println("b) " + (a++))
+  println("c) " + (-2*(g-k2)+c2))
+  //println("d) " + (c2=c2++))
+  //println("e) " + (c2=++c2*a++))
+
+}
